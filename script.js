84b03eef0b7d1c6ffaeff28db7d36cfcc4dbe91f
Anwaar12 <anwaarsalam12@gmail.com>
2023-04-27 00:03:44 +0500

    Adding files to repo

--- script.js
+++ script.js
@@ -1,59 +1,62 @@
-const addButton = document.getElementsByClassName("add-btn")[0];
-const addedSection = document.getElementsByClassName("books")[0];
-let books;
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+}
 
-function display() {
-  if (localStorage.getItem("Books") == null) {
-    books = [];
-  } else {
-    books = JSON.parse(localStorage.getItem("Books"));
+const book = document.getElementsByClassName("books")[0];
+
+class display {
+  static getBooks() {
+    let books;
+    if (localStorage.getItem("books") == null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem("books"));
+    }
+    return books;
   }
 
-  function addBooks(Title, Author) {
-    if (Title !== "" && Author !== "") {
-      var obj = {
-        title: Title,
-        author: Author,
-      };
-      books.push(obj);
-      localStorage.setItem("Books", JSON.stringify(books));
+  static displayBooks() {
+    const books = display.getBooks();
+    let dp = "";
+    books.forEach((book, i) => {
+      dp += `
+              <div class="allbooks">
+              <p>"${book.title}" by ${book.author}</p>
+              <button class="removeBtn" onclick="display.removeBook(${i})">Remove</button>
+              </div>`;
+    });
+    book.innerHTML = dp;
+  }
+
+  static addBook() {
+    const titleInput = document.querySelector("#title").value;
+    const authorInput = document.querySelector("#author").value;
+    if (titleInput !== "" && authorInput !== "") {
+      const newBook = new Book(titleInput, authorInput);
+      const books = display.getBooks();
+      books.push(newBook);
+      localStorage.setItem("books", JSON.stringify(books));
+      this.displayBooks();
     }
   }
 
-  addButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    const title = document.getElementById("title").value.trim();
-    const author = document.getElementById("author").value.trim();
-    addBooks(title, author);
-    display();
-  });
-
-  window.addEventListener("DOMContentLoaded", () => {
-    display();
-  });
-
-  let display = ``;
-  books.forEach((addedSection, i) => {
-    display += `
-        <div>
-        <p>${addedSection.title}</p>
-        <p>${addedSection.author}</p>
-        <button onclick="removeBook(${i})">Remove</button>
-        <hr />
-        </div>
-        `;
-  });
-  addedSection.innerHTML = display;
-}
-
-const removeBook = (id) => {
-  if (localStorage.getItem("Books") == null) {
-    books = [];
-  } else {
-    books = JSON.parse(localStorage.getItem("Books"));
+  static removeBook(id) {
+    const books = display.getBooks();
+    const bookIndex = books.findIndex((item, i) => i === id);
+    books.splice(bookIndex, 1);
+    localStorage.setItem("books", JSON.stringify(books));
+    this.displayBooks();
   }
-  const bookIndex = books.findIndex((item, i) => i === id);
-  books.splice(bookIndex, 1);
-  localStorage.setItem("Books", JSON.stringify(books));
-  display();
-};
+}
+window.addEventListener("DOMContentLoaded", () => {
+  display.displayBooks();
+});
+const form = document.querySelector(".form");
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  display.addBook();
+  form.reset();
+});
