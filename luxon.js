5e72f2c21da6d8c577f85648c4d92a498be8f057
Lethabo Motloung <106984562+LethaboMot@users.noreply.github.com>
2023-05-02 18:30:22 +0200

    defined date time

--- luxon.js
+++ luxon.js
@@ -1,5 +1,6 @@
-import {dateTime} from './index.js';
+import { dateTime } from './index.js';
 
 const timeNow = new Date();
+const dateTime = document.querySelector('.date-time');
 timeNow();
 dateTime();
