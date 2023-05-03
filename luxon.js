1dcda914ce0b44ee6e01080094dc50576ec1d8bb
Lethabo Motloung <106984562+LethaboMot@users.noreply.github.com>
2023-05-03 14:50:13 +0200

    Update luxon.js

--- luxon.js
+++ luxon.js
@@ -1,5 +1,24 @@
-const newDate = DateTime.fromISO(value)
-  .setLocale(LuxonSettings.defaultLocale)
-  .toLocaleString('.date-time');
-  DateTime.fromISO('20123-05-15');
-  DateTime.fromISO('2023-05-15T08:30:00');
+require('.date-time').config();
+const { DateTime } = require('luxon');
+
+const getTime = () => {
+  const ut2 = DataTime.local().setZone('South Africa/Johannesburg');
+  return ut2.toLocalString(DateTime.DATETIME_FULL);
+}
+
+const timeMath = () => {
+  const first = DateTime.local().setZone('South Africa/Johannesburg')
+    let second = DataTime.local()
+    .setZone('South Africa/Johannesburg')
+    .plus({ hours: 1, minutes: 20});
+
+    const diffTime = second.diff(first, ['hours','minutes','seconds']);
+};
+
+const convertISO = () => {
+   const originalISO =  '2023-05-18'
+
+   const formatted = DataTime.fromISO(originalISO, {
+    zone: 'South Africa/Johannesburg'
+   })
+}
