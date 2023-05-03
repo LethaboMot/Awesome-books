1299975a5db720caed9c62dd78751d36b18d9866
Lethabo Motloung <106984562+LethaboMot@users.noreply.github.com>
2023-05-03 02:24:36 +0200

    Update luxon.js

--- luxon.js
+++ luxon.js
@@ -1,4 +1,5 @@
-import { dateTime } from './index.js';
+import {  } from './index.js';
+
 const getTime = () => {
   const utc2 = dateTime.local().setZone('South Africa/Johannesburg');
   return utc2.toLocalString(dateTime.DATETIME_FULL);
