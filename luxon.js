ae6351c5690efbb519f0441e415a0d067cceb9b5
Lethabo Motloung <106984562+LethaboMot@users.noreply.github.com>
2023-05-03 21:27:19 +0200

    Update luxon.js

--- luxon.js
+++ luxon.js
@@ -1,4 +1,5 @@
-import DateTime from 'luxon';
+import DateTime from 'luxon.js';
+
 const currentTime = DateTime.local();
 const formattedTime = `The current time is ${currentTime.toLocaleString(DateTime.TIME_SIMPLE)} using Luxon.`;
 document.getElementsByClassName('.date-time').innerHTML = formattedTime;
