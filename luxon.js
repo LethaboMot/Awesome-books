c683f2d9a654eb430e23a75e9ff4ec53061f5de8
Lethabo Motloung <106984562+LethaboMot@users.noreply.github.com>
2023-05-03 13:14:41 +0200

    Update luxon.js

--- luxon.js
+++ luxon.js
@@ -1,5 +1,5 @@
 const newDate = DateTime.fromISO(value)
   .setLocale(LuxonSettings.defaultLocale)
   .toLocaleString('.date-time');
-  DateTime.fromISO("20123-05-15")
-  DateTime.fromISO("2023-05-15T08:30:00")
+  DateTime.fromISO('20123-05-15');
+  DateTime.fromISO('2023-05-15T08:30:00');
