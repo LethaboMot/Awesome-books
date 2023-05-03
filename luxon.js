5bdd2a53ddb476d8e3f095f86984f90aa06c3254
Lethabo Motloung <106984562+LethaboMot@users.noreply.github.com>
2023-05-03 16:00:24 +0200

    Update luxon.js

--- luxon.js
+++ luxon.js
@@ -1,11 +1,12 @@
-const { DateTime } = require("luxon.js");
+const { DateTime } = require('luxon.js');
 
-const local = DateTime.local();
-local.zoneName; 
-local.toString(); 
-
-const iso = DateTime.fromISO('2017-05-15T09:10:23');
-iso.zoneName; 
-iso.toString(); 
-
+const local = DateTime.local(); {
+  local.zoneName; 
+  local.toString();
+}
+ 
+const iso = DateTime.fromISO('2017-05-15T09:10:23');{
+  iso.zoneName; 
+  iso.toString(); 
+}
 DateTime();
