72874cfd68bd4b1745cabb3ee40aa04a861b2f32
Lethabo Motloung <106984562+LethaboMot@users.noreply.github.com>
2023-05-03 11:25:09 +0200

    Update luxon.js

--- luxon.js
+++ luxon.js
@@ -1,5 +1,2 @@
-import { dateTime } from './index.js';
-const { DateTime } = require('luxon');
-BookInstanceSchema.virtual("due_back_formatted").get(function () {
-  return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
-});
+const d = new Date();
+document.getElementById('.date-time').innerHTML = d;
