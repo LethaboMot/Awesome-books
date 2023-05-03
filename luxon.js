a919a283f312e6e69b2a9413742b3d01df97a8c0
Lethabo Motloung <106984562+LethaboMot@users.noreply.github.com>
2023-05-03 15:42:09 +0200

    Update luxon.js

--- luxon.js
+++ luxon.js
@@ -1,8 +1,9 @@
-let output = document.querySelector('.date-time');
- output.textContent = JSON.stringify(luxon.DateTime.DATETIME_FULL);
+const local = DateTime.local(2017, 05, 15, 09, 10, 23);
 
- let DateTime = luxon.DateTime;
- let today = DateTime.local();
- let f = {month: 'long', day: '2-digit'};
- let m = today.get('month');
- let newDt = today.set({month: 12});
+local.zoneName; 
+local.toString(); 
+
+var iso = DateTime.fromISO("2017-05-15T09:10:23");
+
+iso.zoneName; 
+iso.toString(); 
