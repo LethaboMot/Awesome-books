39708c5329cabf9448b0871b4f8539257ccb132c
Lethabo Motloung <106984562+LethaboMot@users.noreply.github.com>
2023-05-04 04:33:38 +0200

    Update luxon.js

--- luxon.js
+++ luxon.js
@@ -2,7 +2,7 @@ const { DateTime } = require('./index.js');
 
 const currentDateTime = DateTime.local().toLocaleString(DateTime.TIME_WITH_SECONDS);
 const currentTimeElement = document.getElementById('current-time');
-const timeNow = dateTime();
+const timeNow = null();
 currentTimeElement.innerHTML = currentDateTime;
 
 const dateTime = document.querySelector('.date-time');
